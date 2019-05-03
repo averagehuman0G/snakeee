f0221ebf9aecfb1b0a6cfe1e905628c35e2b4ce4
Jonathan Vargas <jonathanv3232@gmail.com>
2019-05-03 16:53:13 -0400

    draws the food

--- index.js
+++ index.js
@@ -3,20 +3,53 @@
 const canvas = document.getElementById('snake-canvas');
 const ctx = canvas.getContext('2d');
 const squareSize = 64;
-const gridSize = 608;
+const gridSize = 640;
+const squaresInGrid = gridSize / squareSize;
+
+
+const pinUrl = 'pin.png';
+const pin = new Image();
+pin.onload = function () {
+    makeFood();
+}
+pin.src = pinUrl;
+
+function snakeNode(x, y) {
+    this.x = x;
+    this.y = y;
+    this.next = null;
+}
+
+function makeFood() {
+    const maxNumberOfFood = 10;
+    const randomNumberOfFood = Math.floor(Math.random() * maxNumberOfFood + 1);
+    for(let i = 0; i < 10; i++) {
+        const randomX = Math.floor(Math.random() * squaresInGrid) * squareSize;    
+        const randomY = Math.floor(Math.random() * squaresInGrid) * squareSize;
+        console.log(randomX, randomY);
+        ctx.drawImage(pin, randomX, randomY);
+    }
+}
 
 function drawSquare(x, y) {
     ctx.strokeRect(x, y, squareSize, squareSize);
 }
+
 
 for(let i = 0; i < gridSize; i += squareSize) {
     for(let j = 0; j < gridSize; j+= squareSize) {
         drawSquare(i, j);
     }
 }
+makeFood();
+
 const startX = 0;
 const startY = 0;
 
 function drawSnake() {
     
+    //iterate through the list drawing a rectangle and filling the rectangle    
+
 }
+
+let game = setInterval(drawSnake, 100);
